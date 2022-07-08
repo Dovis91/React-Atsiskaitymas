2d38c7824a2c9fe4bbb51ffe6431b80f88201461
Dovis91 <dovydas.ziukas@gmail.com>
2022-07-08 11:23:52 +0300

    app jsx + readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# Project is made with React front-end, used API:
+
+BASE_URL = https://autumn-delicate-wilderness.glitch.me/
+
+REGISTER = https://autumn-delicate-wilderness.glitch.me/v1/auth/register
+
+LOGIN = https://autumn-delicate-wilderness.glitch.me/v1/auth/login
+
+HOME = https://autumn-delicate-wilderness.glitch.me/v1/content/skills
+
+ADD = https://autumn-delicate-wilderness.glitch.me/v1/content/skills.
+
+### To run project
+
+1. You have to clone github repository.
+2. `npm run install`
+3. `npm run start` and Open [http://localhost:3000] to view it in your browser
+
+The page will reload when you make changes.
+You may also see any lint errors in the console or alerts on main screen.
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import Navbar from './components/Navbar';
+import Register from './components/Register';
+import Login from './components/Login';
+import Home from './components/Home';
+import Add from './components/Add';
 
 function App() {
+  const navigate = useNavigate();
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setUserLoggedIn(true);
+      navigate('/');
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <>
+      <Navbar userLoggedIn={userLoggedIn} setUserLoggedIn={setUserLoggedIn} />
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/login"
+          element={<Login setUserLoggedIn={setUserLoggedIn} />}
+        />
+        <Route path="/add" element={<Add />} />
+        <Route
+          path="/"
+          element={
+            <Home
+              userLoggedIn={userLoggedIn}
+              setUserLoggedIn={setUserLoggedIn}
+            />
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
